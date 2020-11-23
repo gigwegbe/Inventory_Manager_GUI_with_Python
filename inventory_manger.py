e9b50db3c7fd73aaac85c483b27d4b1af18274ac
george <gigwegbe@gmail.com>
2020-11-23 17:42:19 +0100

    Load UI into the script file

--- inventory_manger.py
+++ inventory_manger.py
@@ -14,3 +14,23 @@ from PyQt5.uic import loadUiType
 FORM_CLASS, _ = loadUiType(path.join(path.dirname('__file__'), "main.ui"))
 
 import sqlite3 
+
+class Main(QMainWindow, FORM_CLASS): 
+    def __init__(self, parent=None):
+        super(Main, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.Handel_Buttons()
+    
+    def Handel_Buttons(self):
+        pass 
+
+def main(): 
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+
+
+if __name__=='__main__': 
+    main()
